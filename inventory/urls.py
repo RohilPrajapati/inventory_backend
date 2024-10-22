f74afdf9e0b47787f633d7660004ce0824cf4bc3
RohilPrajapati <rohilprajapati@gmail.com>
2024-10-22 21:43:01 +0545

    fix url class name miss match

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', TransactionListView.as_view(),name='transaction_list'),
     path('stock/', InventoryListView.as_view(),name='transaction_list'),
-    path('stock/product/<int:product_id>/', InventoryListView.as_view(),name='transaction_list'),
+    path('stock/product/<int:product_id>/', ProductWiseInventoryView.as_view(),name='transaction_list'),
     path('purchase/', CreatePurchaseView.as_view(),name='create_purchase'),
     path('sales/', CreateSalesView.as_view(),name='create_sales'),
 ]
