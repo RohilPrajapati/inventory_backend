f221acfbbcbf574b1bbbf64b77f2c6be2eacb1fb
RohilPrajapati <rohilprajapati@gmail.com>
2025-02-23 21:02:44 +0545

    fix is admin permission class from token

--- backend/permissions.py
+++ backend/permissions.py
@@ -1,14 +1,18 @@
 from rest_framework import permissions
+from users.tokens import decode_token
 
 
 class IsAdminUser(permissions.BasePermission):
+    """
+    Custom permission to grant access only to admin users.
+    """
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
+        """Check if the user is authenticated and has an admin role."""
+        if not request.user or not request.user.is_authenticated:
             return False
-        if request.user.role_id.role_id == 1 or request.user.role_id == 1:
-            return True
-        else:
-            return False
+
+        payload = decode_token(request)
+        return payload.get('role') == 'admin'
 
 # staff ko permission 
